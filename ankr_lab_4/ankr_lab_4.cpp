a615c0e62f82482d3ce4fcbd4d1c00fb77923bb5
dexXxed <dexXxed.app@gmail.com>
2019-12-19 12:39:03 +0200

    lklklklkll

--- ankr_lab_4/ankr_lab_4.cpp
+++ ankr_lab_4/ankr_lab_4.cpp
@@ -122,6 +122,7 @@ void set_key_info(HCRYPTKEY key_handler, const block_key_info& info)
 }
 
 
+
 void get_hash(const char* filename, HCRYPTPROV csp_handler, ALG_ID alg_id, HCRYPTKEY key_handler, HCRYPTHASH& hash_handler)
 {
 	if (!CryptCreateHash(csp_handler, alg_id, key_handler, 0, &hash_handler)) {
@@ -140,11 +141,34 @@ void get_hash(const char* filename, HCRYPTPROV csp_handler, ALG_ID alg_id, HCRYP
 	fclose(f);
 }
 
+void gen_sblock_key(HCRYPTPROV csp_handler, DWORD alg_id, HCRYPTKEY& key_handler, DWORD mode)
+{
+	csp_alg_properties alg_prop;
+	get_alg_properties(csp_handler, alg_id, alg_prop);
+	DWORD keylen = alg_prop.enumalgs.dwMaxLen;
+	DWORD flags = keylen << 16;
+	flags |= CRYPT_EXPORTABLE;
+	flags |= CRYPT_USER_PROTECTED;
+	if (!CryptGenKey(csp_handler, alg_id, flags, &key_handler))
+		throw descriptive_exception("in key create");
+	block_key_info info;
+	info.mode = mode;
+	DWORD dword_size = sizeof(DWORD);
+	if (!CryptGetKeyParam(key_handler, KP_BLOCKLEN, (BYTE*)&(info.block_byte_size), &dword_size, 0))
+		throw descriptive_exception("in get key block size");
+	info.iv = new BYTE[info.block_byte_size];
+	if (!CryptGenRandom(csp_handler, info.block_byte_size, info.iv))
+		throw descriptive_exception("in gen iv");
+	set_key_info(key_handler, info);
+	delete[] info.iv;
+}
+
 void sign_file(const char* filename, HCRYPTPROV csp_handler, ALG_ID hash_id)
 {
 	HCRYPTHASH hash_handler = 0;
 	HCRYPTKEY key_handler = 0;
 	get_hash(filename, csp_handler, hash_id, key_handler, hash_handler);
+
 	DWORD sign_len;
 	if (!CryptSignHash(hash_handler, AT_SIGNATURE, NULL, 0, NULL, &sign_len))
 		throw descriptive_exception("get sign len");
@@ -161,6 +185,45 @@ void sign_file(const char* filename, HCRYPTPROV csp_handler, ALG_ID hash_id)
 	fwrite(sign_data, 1, sign_len, f);
 	fclose(f);
 	delete[] sign_data;
+}
+
+void get_key_info(HCRYPTKEY key_handler, block_key_info& info)
+{
+	DWORD dword_size = sizeof(DWORD);
+	if (!CryptGetKeyParam(key_handler, KP_MODE, (BYTE*)&(info.mode), &dword_size, 0))
+		throw descriptive_exception("in get key mode");
+	if (!CryptGetKeyParam(key_handler, KP_BLOCKLEN, (BYTE*)&(info.block_byte_size), &dword_size, 0))
+		throw descriptive_exception("in get key block size");
+	info.block_byte_size /= 8;
+	info.iv = new BYTE[info.block_byte_size];
+	if (!CryptGetKeyParam(key_handler, KP_IV, info.iv, &(info.block_byte_size), 0))
+		throw descriptive_exception("in get key block");
+}
+
+void export_key(HCRYPTKEY key_handler, HCRYPTKEY expkey_handler, const char* filename)
+{
+	DWORD blob_size;
+	if (!CryptExportKey(key_handler, expkey_handler, SIMPLEBLOB, 0, NULL, &blob_size))
+		throw descriptive_exception("in get blob size");
+	BYTE* blob = new BYTE[blob_size];
+	if (!CryptExportKey(key_handler, expkey_handler, SIMPLEBLOB, 0, blob, &blob_size))
+		throw descriptive_exception("in get blob");
+	FILE* f = fopen(filename, "wb");
+	if (!f) throw descriptive_exception("in open file to write");
+	if (fwrite(&blob_size, 1, sizeof(blob_size), f) != sizeof(blob_size))
+		throw descriptive_exception("in writing to file");
+	if (fwrite(blob, 1, blob_size, f) != blob_size)
+		throw descriptive_exception("in writing to file");
+	delete[] blob;
+	block_key_info info;
+	get_key_info(key_handler, info);
+	if (fwrite(&(info.mode), 1, sizeof(info.mode), f) != sizeof(info.mode))
+		throw descriptive_exception("in writing to file");
+	if (fwrite(&(info.block_byte_size), 1, sizeof(info.block_byte_size), f) != sizeof(info.block_byte_size))
+		throw descriptive_exception("in writing to file");
+	if (fwrite(info.iv, 1, info.block_byte_size, f) != info.block_byte_size)
+		throw descriptive_exception("in writing to file");
+	fclose(f);
 }
 
 bool verify_file(const char* filename, HCRYPTPROV csp_handler, ALG_ID hash_id)
@@ -191,8 +254,10 @@ int main(int argc, const char** argv)
 {
 	DWORD csp_type = PROV_RSA_AES;
 	auto csp_name = (LPTSTR) MS_ENH_RSA_AES_PROV;
-	std::string keyset_name = "dexxxed";
-	ALG_ID hash_id = CALG_SHA; 
+	std::string keyset_name = "dexxccccxed";
+	ALG_ID hash_id = 32773; 
+	DWORD alg_sblock_id = 26128; // AES 256-bit
+
 
 	HCRYPTPROV csp_handler = 0;
 
@@ -207,9 +272,14 @@ int main(int argc, const char** argv)
 	}
 	try
 	{
-		get_csp_handler(csp_type, csp_name, keyset_name, csp_handler);
+		
 		if (!strcmp(argv[1], "sign"))
 		{
+			HCRYPTKEY key_handler, expkey_handler;
+			gen_sblock_key(csp_handler, alg_sblock_id, key_handler, CRYPT_MODE_OFB);
+			if (!CryptGetUserKey(csp_handler, AT_KEYEXCHANGE, &expkey_handler))
+				throw descriptive_exception("get exchange key");
+			export_key(key_handler, expkey_handler, argv[3]);
 			sign_file(argv[2], csp_handler, hash_id);
 		}
 		else if (!strcmp(argv[1], "verify"))
